44e0da0c1f0f1a53e834cda1c7b3fabbb80dbc6c
kabanidze123 <kaban0616@gmail.com>
2025-10-16 09:09:46 +0700

    Add files via upload

---
+++ ЛР№3_3_Долгушин/ЛР№3_3_Долгушин/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ЛР_3_3_Долгушин
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(436, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вывести текст на метку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Место для вывода текста";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(87, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(436, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стереть текст с метки";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(87, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(436, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Сделать кнопку недоступной";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(87, 302);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(436, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Сделать кнопку доступной";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(87, 373);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(436, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Сделать кнопку невидимой";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(87, 444);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(436, 49);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Сделать кнопку видимой";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 566);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Управление свойствами кнопок";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ЛР№3_3_Долгушин/ЛР№3_3_Долгушин/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛР_3_3_Долгушин
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Текст выведен на метку!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button5.Visible = true;
+        }
+    }
+}
